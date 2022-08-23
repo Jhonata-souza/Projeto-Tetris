53ce72cfc2f9a0c5ed723585f416ea14ae766725
Jhonata-souza <101649107+Jhonata-souza@users.noreply.github.com>
2022-08-23 10:40:19 -0300

    continuação do projeto Tetris

---
+++ Projeto-Tetris-main/Projeto-Tetris-main/Jn e Di/Projeto Tetris/js/painel.js
@@ -0,0 +1,13 @@
+class Painel{
+    linhas;
+    matrizes;
+    celulas;
+    colunas;
+    constructor(linhas, matrizes, celulas, colunas) {
+        this.linhas = linhas;
+        this.matrizes = matrizes;
+        this.celulas = celulas;
+        this.colunas = colunas;
+    }
+
+}
